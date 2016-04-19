b7520c6b7491acfc2175b7364b6c8948f555f128
Andres Peña Luna <Andres Pea Luna>
2016-04-19 00:20:39 -0300

    ceil removed

--- public_html/assets/js/Entities.js
+++ public_html/assets/js/Entities.js
@@ -63,7 +63,7 @@ var CarManager = function () {
             jq_obj.addClass("car_render");
             jq_obj.css("left", typeof c.position.x !== 'number' ? 0:c.position.x + 3);
             // todo: some hardcode here
-            jq_obj.css("top",c.position.y === 'auto'?  Math.ceil((parseInt(c.year) - 1945) * (1161 - 15) / 70 + 23) : c.position.y);
+            jq_obj.css("top",c.position.y === 'auto'? (parseInt(c.year) - 1945) * (1161 - 15) / 70 + 23 : c.position.y);
             jq_obj.css("position", "absolute");
             $("#" + _render_on_id).append(jq_obj);
         });
